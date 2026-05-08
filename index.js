1c08511e8d2e6f84314e7e777f699d0e58b85737
math037 <math037@live.fr>
2026-05-08 18:06:59 +0200

    Initial commit

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
+client.on('ready', () => console.log(`Bot connecté en tant que ${client.user.tag}`));
+
+client.on('messageCreate', (message) => {
+  if (message.content === 'ping') message.reply('pong');
+});
+
+client.login(process.env.DISCORD_TOKEN);
